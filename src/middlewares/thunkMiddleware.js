3df136ce2308bbe15c2d3ff7d8619a3bd2ccc481
Nikolay Kostyurin <jilizart@gmail.com>
2015-09-12 03:37:34 +0200

    thunk middleware

---
+++ src/middlewares/thunkMiddleware.js
@@ -0,0 +1,15 @@
+import { isFSA } from 'flux-standard-action';
+
+const isFunction = (func) => typeof func === 'function';
+
+export default function thunkMiddleware({ dispatch, getState }) {
+  return (next) => {
+    return (action) => {
+      if (isFSA(action)) {
+        return isFunction(action.payload) ? action.payload(dispatch, getState) : next(action);
+      } else {
+        return isFunction(action) ? action(dispatch, getState) : next(action);
+      }
+    }
+  }
+}
